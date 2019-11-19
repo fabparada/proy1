128142b05fb779c10e9fe182e80e35ad4a34aac5
Fabian Parada <fparada@digital.gob.cl>
2019-11-19 14:45:53 -0300

    se añade funcionalidad de aumentar y v-bind

--- src/components/Lista.vue
+++ src/components/Lista.vue
@@ -1,24 +1,33 @@
 <template>
     <div>
         <ul class="list-group">
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                Cras justo odio
-                <span class="badge badge-primary badge-pill">14</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                Dapibus ac facilisis in
-                <span class="badge badge-primary badge-pill">2</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                Morbi leo risus
-                <span class="badge badge-primary badge-pill">1</span>
+            <li v-for="(item, index) of arrayOrdenado" :key="item.id" @click="aumentar(index)" v-bind:class="{ 'bg-success': item.estado, 'text-dark': !item.estado }"
+            class="list-group-item d-flex justify-content-between align-items-center">
+               {{index}} - {{ item.nombre }}
+                <span class="badge badge-primary badge-pill">{{item.cantidad}}</span>
             </li>
         </ul>
+        <div>
+           <button type="button" class="btn btn-block btn-secondary" @click="resetear">reiniciar</button>
+        </div>
     </div>
 </template>
 
 <script>
+import {mapState, mapMutations} from 'vuex';
+
+
 export default {
-    name: 'Lista'
+    name: 'Lista',
+    computed: {
+        ...mapState(['frutas']),
+        arrayOrdenado(){
+            return this.frutas.sort( (a, b) => b.cantidad - a.cantidad )
+        }
+    },
+    methods: {
+        ...mapMutations(['aumentar','resetear']),
+        
+    }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    frutas: [
+      {nombre: 'manzana', cantidad: 0, estado: false},
+      {nombre: 'pera', cantidad: 0, estado: false}, 
+      {nombre: 'naranja', cantidad: 0, estado: false}
+    ]
   },
   mutations: {
+    aumentar(state, index){
+      state.frutas[index].cantidad ++;
+      state.frutas[index].estado = true;
+    },
+    resetear(state) {
+      if(state.frutas.length > 0){
+        state.frutas.forEach(elemento => {
+          elemento.cantidad = 0;
+          elemento.estado = false
+        })
+      }  
+      else{
+        console.log("no hay frutas en la canasta");
+      }
+    }
+  
   },
   actions: {
   },
